3f0f988cdf6dccec46dbde10196733678b023ac7
Salvador Bascunan <salvador.bascunan@tieto.com>
2026-06-08 22:17:31 +0200

    delegate more from AsyncApiGenerator

--- asyncapi-generator-core/src/main/kotlin/dev/banking/asyncapi/generator/core/generator/AsyncApiGenerator.kt
+++ asyncapi-generator-core/src/main/kotlin/dev/banking/asyncapi/generator/core/generator/AsyncApiGenerator.kt
@@ -1,18 +1,11 @@
 package dev.banking.asyncapi.generator.core.generator
 
-import dev.banking.asyncapi.generator.core.generator.avro.AvroGenerator
-import dev.banking.asyncapi.generator.core.generator.input.GenerationInput
+import dev.banking.asyncapi.generator.core.generator.artifact.AvroSchemaArtifactGeneration
+import dev.banking.asyncapi.generator.core.generator.artifact.ModelArtifactGeneration
 import dev.banking.asyncapi.generator.core.generator.input.GenerationInputFactory
-import dev.banking.asyncapi.generator.core.generator.java.JavaGenerator
-import dev.banking.asyncapi.generator.core.generator.java.JavaModelPreparer
 import dev.banking.asyncapi.generator.core.generator.kafka.spring.SpringKafkaClientGeneration
-import dev.banking.asyncapi.generator.core.generator.kotlin.KotlinGenerator
-import dev.banking.asyncapi.generator.core.generator.kotlin.KotlinModelPreparer
-import dev.banking.asyncapi.generator.core.generator.model.GeneratorName.JAVA
-import dev.banking.asyncapi.generator.core.generator.model.GeneratorName.KOTLIN
 import dev.banking.asyncapi.generator.core.generator.model.GeneratorOptions
 import dev.banking.asyncapi.generator.core.generator.output.FileSystemGeneratedArtifactWriter
-import dev.banking.asyncapi.generator.core.generator.output.GeneratedArtifactWriter
 import dev.banking.asyncapi.generator.core.generator.plan.GenerationPlanner
 import dev.banking.asyncapi.generator.core.generator.plan.GenerationTask
 import dev.banking.asyncapi.generator.core.model.asyncapi.AsyncApiDocument
@@ -28,9 +21,9 @@ class AsyncApiGenerator {
     private val log = LoggerFactory.getLogger(AsyncApiGenerator::class.java)
 
     private val generationInputFactory = GenerationInputFactory()
-    private val kotlinModelPreparer = KotlinModelPreparer()
-    private val javaModelPreparer = JavaModelPreparer()
     private val generationPlanner = GenerationPlanner()
+    private val modelArtifactGeneration = ModelArtifactGeneration()
+    private val avroSchemaArtifactGeneration = AvroSchemaArtifactGeneration()
     private val springKafkaClientGeneration = SpringKafkaClientGeneration()
 
     fun generate(
@@ -48,18 +41,18 @@ class AsyncApiGenerator {
         generationPlan.tasks.forEach { task ->
             when (task) {
                 is GenerationTask.ModelArtifacts ->
-                    writeModelArtifacts(
+                    modelArtifactGeneration.generateModelArtifacts(
                         task = task,
                         generationInput = generationInput,
-                        generatorOptions = generatorOptions,
+                        sourceOutputDirectory = generatorOptions.codegenOutputDirectory,
                         artifactWriter = artifactWriter,
                     )
                 is GenerationTask.HeaderModelArtifacts ->
-                    writeHeaderModelArtifacts(
+                    modelArtifactGeneration.generateHeaderModelArtifacts(
                         task = task,
                         asyncApiDocument = asyncApiDocument,
                         generationInput = generationInput,
-                        generatorOptions = generatorOptions,
+                        sourceOutputDirectory = generatorOptions.codegenOutputDirectory,
                         artifactWriter = artifactWriter,
                     )
                 is GenerationTask.SpringKafkaClient ->
@@ -72,112 +65,14 @@ class AsyncApiGenerator {
                 is GenerationTask.QuarkusKafkaClient ->
                     log.info("Generate ${task.language.name.titlecase()} Quarkus Kafka Client is not yet implemented. Skipping..")
                 is GenerationTask.AvroSchemaArtifacts ->
-                    writeAvroSchemaArtifacts(
+                    avroSchemaArtifactGeneration.generate(
                         task = task,
                         generationInput = generationInput,
-                        generatorOptions = generatorOptions,
+                        resourceOutputDirectory = generatorOptions.resourceOutputDirectory,
                         artifactWriter = artifactWriter,
                     )
             }
         }
-    }
-
-    private fun writeModelArtifacts(
-        task: GenerationTask.ModelArtifacts,
-        generationInput: GenerationInput,
-        generatorOptions: GeneratorOptions,
-        artifactWriter: GeneratedArtifactWriter,
-    ) {
-        when (task.language) {
-            KOTLIN -> {
-                val generationModel =
-                    kotlinModelPreparer.prepare(
-                        input = generationInput,
-                        packageName = task.packageName,
-                        annotation = task.annotation,
-                    )
-                val generator =
-                    KotlinGenerator(
-                        packageName = task.packageName,
-                        outputDir = generatorOptions.codegenOutputDirectory,
-                        generationModel = generationModel,
-                    )
-                artifactWriter.write(generator.render())
-            }
-            JAVA -> {
-                val generationModel =
-                    javaModelPreparer.prepare(
-                        input = generationInput,
-                        packageName = task.packageName,
-                    )
-                val generator =
-                    JavaGenerator(
-                        packageName = task.packageName,
-                        outputDir = generatorOptions.codegenOutputDirectory,
-                        generationModel = generationModel,
-                    )
-                artifactWriter.write(generator.render())
-            }
-        }
-    }
-
-    private fun writeHeaderModelArtifacts(
-        task: GenerationTask.HeaderModelArtifacts,
-        asyncApiDocument: AsyncApiDocument,
-        generationInput: GenerationInput,
-        generatorOptions: GeneratorOptions,
-        artifactWriter: GeneratedArtifactWriter,
-    ) {
-        when (task.language) {
-            KOTLIN -> {
-                val headerModels =
-                    kotlinModelPreparer.prepareHeaders(
-                        input = generationInput,
-                        asyncApiDocument = asyncApiDocument,
-                        packageName = task.packageName,
-                    )
-                if (headerModels.isNotEmpty()) {
-                    val generator =
-                        KotlinGenerator(
-                            packageName = task.packageName,
-                            outputDir = generatorOptions.codegenOutputDirectory,
-                            generationModel = headerModels,
-                        )
-                    artifactWriter.write(generator.render())
-                }
-            }
-            JAVA -> {
-                val headerModels =
-                    javaModelPreparer.prepareHeaders(
-                        input = generationInput,
-                        asyncApiDocument = asyncApiDocument,
-                        packageName = task.packageName,
-                    )
-                if (headerModels.isNotEmpty()) {
-                    val generator =
-                        JavaGenerator(
-                            packageName = task.packageName,
-                            outputDir = generatorOptions.codegenOutputDirectory,
-                            generationModel = headerModels,
-                        )
-                    artifactWriter.write(generator.render())
-                }
-            }
-        }
-    }
-
-    private fun writeAvroSchemaArtifacts(
-        task: GenerationTask.AvroSchemaArtifacts,
-        generationInput: GenerationInput,
-        generatorOptions: GeneratorOptions,
-        artifactWriter: GeneratedArtifactWriter,
-    ) {
-        val avroGenerator =
-            AvroGenerator(
-                outputDir = generatorOptions.resourceOutputDirectory,
-                packageName = task.packageName,
-            )
-        artifactWriter.write(avroGenerator.render(generationInput.schemas))
     }
 
     private fun String.titlecase(): String =
---
+++ asyncapi-generator-core/src/main/kotlin/dev/banking/asyncapi/generator/core/generator/artifact/ModelArtifactGeneration.kt
@@ -0,0 +1,108 @@
+package dev.banking.asyncapi.generator.core.generator.artifact
+
+import dev.banking.asyncapi.generator.core.generator.input.GenerationInput
+import dev.banking.asyncapi.generator.core.generator.java.JavaGenerator
+import dev.banking.asyncapi.generator.core.generator.java.JavaModelPreparer
+import dev.banking.asyncapi.generator.core.generator.kotlin.KotlinGenerator
+import dev.banking.asyncapi.generator.core.generator.kotlin.KotlinModelPreparer
+import dev.banking.asyncapi.generator.core.generator.model.GeneratorName.JAVA
+import dev.banking.asyncapi.generator.core.generator.model.GeneratorName.KOTLIN
+import dev.banking.asyncapi.generator.core.generator.output.GeneratedArtifactWriter
+import dev.banking.asyncapi.generator.core.generator.plan.GenerationTask
+import dev.banking.asyncapi.generator.core.model.asyncapi.AsyncApiDocument
+import java.io.File
+
+/**
+ * Renders planned Kotlin and Java model artifacts before writing them.
+ *
+ * Expected behavior is covered by:
+ * - `ModelArtifactGenerationTest`
+ */
+class ModelArtifactGeneration(
+    private val kotlinModelPreparer: KotlinModelPreparer = KotlinModelPreparer(),
+    private val javaModelPreparer: JavaModelPreparer = JavaModelPreparer(),
+) {
+    fun generateModelArtifacts(
+        task: GenerationTask.ModelArtifacts,
+        generationInput: GenerationInput,
+        sourceOutputDirectory: File,
+        artifactWriter: GeneratedArtifactWriter,
+    ) {
+        when (task.language) {
+            KOTLIN -> {
+                val generationModel =
+                    kotlinModelPreparer.prepare(
+                        input = generationInput,
+                        packageName = task.packageName,
+                        annotation = task.annotation,
+                    )
+                val generator =
+                    KotlinGenerator(
+                        packageName = task.packageName,
+                        outputDir = sourceOutputDirectory,
+                        generationModel = generationModel,
+                    )
+                artifactWriter.write(generator.render())
+            }
+            JAVA -> {
+                val generationModel =
+                    javaModelPreparer.prepare(
+                        input = generationInput,
+                        packageName = task.packageName,
+                    )
+                val generator =
+                    JavaGenerator(
+                        packageName = task.packageName,
+                        outputDir = sourceOutputDirectory,
+                        generationModel = generationModel,
+                    )
+                artifactWriter.write(generator.render())
+            }
+        }
+    }
+
+    fun generateHeaderModelArtifacts(
+        task: GenerationTask.HeaderModelArtifacts,
+        asyncApiDocument: AsyncApiDocument,
+        generationInput: GenerationInput,
+        sourceOutputDirectory: File,
+        artifactWriter: GeneratedArtifactWriter,
+    ) {
+        when (task.language) {
+            KOTLIN -> {
+                val headerModels =
+                    kotlinModelPreparer.prepareHeaders(
+                        input = generationInput,
+                        asyncApiDocument = asyncApiDocument,
+                        packageName = task.packageName,
+                    )
+                if (headerModels.isNotEmpty()) {
+                    val generator =
+                        KotlinGenerator(
+                            packageName = task.packageName,
+                            outputDir = sourceOutputDirectory,
+                            generationModel = headerModels,
+                        )
+                    artifactWriter.write(generator.render())
+                }
+            }
+            JAVA -> {
+                val headerModels =
+                    javaModelPreparer.prepareHeaders(
+                        input = generationInput,
+                        asyncApiDocument = asyncApiDocument,
+                        packageName = task.packageName,
+                    )
+                if (headerModels.isNotEmpty()) {
+                    val generator =
+                        JavaGenerator(
+                            packageName = task.packageName,
+                            outputDir = sourceOutputDirectory,
+                            generationModel = headerModels,
+                        )
+                    artifactWriter.write(generator.render())
+                }
+            }
+        }
+    }
+}
